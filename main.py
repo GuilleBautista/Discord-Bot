79e6e8e1185c84c9ada00a3739c96aab6d710f39
GUILLERMO BAUTISTA-ABAD ACEBES <a0383477@lab.inf.uc3m.es>
2019-11-13 23:53:23 +0100

    hello world

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 import discord
+from discord.ext import commands
 
+TOKEN="jnphZKuvmrPP_gy7u4ksNiirDImCyM7i"
+
+
+client=commands.Bot(command_prefix = "holi")
+
+@client.event
+async def on_ready():
+    print("Ready to go")
+
+@client.command(pass_context=True)
+async def join(ctx):
+    channel = ctx.message.author.voice.voice_channel
+    await client.join_voice_channel(channel)
 
 print("Hello human")
+
+
+client.run(TOKEN)
